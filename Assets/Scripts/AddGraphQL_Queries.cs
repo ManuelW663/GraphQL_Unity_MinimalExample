befafeb2f05c1584bd499bdf8ec3f74b93823e03
Manuel Wurzer <manuel.wurzer@hcu-hamburg.de>
2021-12-04 19:05:47 +0100

    Initial Commit

---
+++ Assets/Scripts/AddGraphQL_Queries.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleGraphQL;
+
+
+public class AddGraphQL_Queries : MonoBehaviour
+{
+    public GraphQLConfig config;
+
+    public Object[] files;
+
+    void Start()
+    {
+        // Laden aller Files im Verzeichnis "GraphQL_Files" in Ordnern namens "Resources",
+        // hier unter "GraphQL_Assets"
+        files = Resources.LoadAll("GraphQL_Files");
+
+        Debug.Log("Files im Resources Ordner: " + files.GetLength(0));
+
+        // Hinzufügen der aufgefundenen Files zu config, notwendig ist ein Cast der Objekte,
+        // welcher bei unpassenden Files im Resources-Ordner zu Fehlern führen wird
+        config.Files.Add((GraphQLFile)files[0]);
+        config.Files.Add((GraphQLFile)files[1]);
+    }
+}
